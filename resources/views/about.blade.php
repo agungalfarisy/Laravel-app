a194b1e3d449651a04de6e8b1c4e84998d950cb3
Agung Alfarisy <agungalfarisy@gmail.com>
2019-12-04 14:15:52 +0700

    Menambahkan Project Laravel-app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+
+@section('judul', 'About')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1>Selamat Datang, Bapak{{$nama}}!</h1>
+        </div>
+    </div>
+</div>
+@endsection
